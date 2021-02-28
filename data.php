87d8f4599d5ff1fb18903470f696cd01f02e83cc
novintannurdelis <79629595+novintannurdelis@users.noreply.github.com>
2021-02-28 17:26:20 +0700

    Add files via upload

--- data.php
+++ data.php
@@ -1,34 +1,30 @@
 <div class='panel panel-border panel-pink'>
                                     <div class='panel-heading'> 
-                                        <h3 class='panel-title'><i class='fa fa-user'></i> Data Karyawan</h3> 
+                                        <h3 class='panel-title'><i class='fa fa-list'></i> Data Pemakaian Barang</h3> 
                                     </div>  <div class='panel-body'> 
                                 <table id='datatable' class='table table-hover'>
                                     <thead>
                                         <tr>
-										<th><i class='icon-terminal'></i> NIK</th>
-                                            <th><i class='icon-terminal'></i> Nama</th>
-                                            <th><i class='icon-time'></i> Username</th>
-											<th><i class='icon-signal'></i> Alamat</th>
-											<th><i class='icon-signal'></i> Telp</th>
-											<th><i class='icon-signal'></i> Gender</th>
+										<th><i class='icon-terminal'></i> No</th>
+                                            <th><i class='icon-terminal'></i> Nama Barang</th>
+                                            <th><i class='icon-time'></i> Jumlah</th>
+											<th><i class='icon-signal'></i> Tanggal Pakai</th>
                                         </tr>
                                     </thead>
                                     <tbody>
 									<?php
 							$i=1;
-							$tp=mysql_query("SELECT * FROM pengguna WHERE level='Karyawan' ORDER BY id ");
+							$tp=mysql_query("SELECT * FROM pemakaian ORDER BY id ");
 							while($r=mysql_fetch_array($tp)){
 							?>
 							<tr>
-							 <td><?php echo $r['nik'];?></td>
-                                    <td><?php echo $r['nama'];?></td>
-									<td><?php echo$r['username'];?></td>
-									<td><?php echo$r['alamat'];?></td>
-									<td><?php echo$r['telp'];?></td>
-									<td><?php echo$r['gender'];?></td>
+							 <td><?php echo $i;?></td>
+                                    <td><?php echo $r['barang'];?></td>
+									<td><?php echo$r['jumlah'];?></td>
+									<td><?php echo TanggalIndo($r['tgl_pakai']);?></td>
                                     
 							</tr>
-							<?php $i=$i+1;?>
+						<?php $i=$i+1;?>
 							<?php } ?>
 							</tbody>
                         </table>
